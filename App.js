cefe3dea871ab67a8905550acc0c7a2b51a8af8a
Eldar Terzic <eldar.terzic@insum.se>
2022-12-20 20:50:27 +0100

    remove items from list

--- App.js
+++ App.js
@@ -14,12 +14,21 @@ export default function App() {
     }]);    
   };
 
+  function deleteGoalHandler(id){
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler}></GoalInput>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={itemData => {
-          return <GoalItem text={itemData.item.text}></GoalItem>
+          return <GoalItem 
+            text={itemData.item.text} 
+            id={itemData.item.id}
+            onDeleteItem={deleteGoalHandler}></GoalItem>
           }}
           keyExtractor={(item, index) => {
             return item.id;
